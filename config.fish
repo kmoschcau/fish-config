6d0ba449e5586fd39974109424ca97255d0c91af
Kai Moschcau <kai.moschcau@sabre.com>
2018-02-07 18:44:24 +0100

    Add basic fish configuration files

---
+++ config.fish
@@ -0,0 +1,8 @@
+# vim: foldmethod=marker
+
+# powerline | powerline/powerline {{{1
+
+set fish_function_path $fish_function_path "$HOME/.pyenv/versions/3.6.3/lib/python3.6/site-packages/powerline/bindings/fish"
+powerline-setup
+
+# }}}1
