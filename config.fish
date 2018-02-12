87e2623a5bb038ea632be0a23a87d7291a2d3159
Kai Moschcau <kai.moschcau@sabre.com>
2018-02-12 17:22:17 +0100

    Disable the fish vi mode prompt

--- config.fish
+++ config.fish
@@ -8,6 +8,9 @@ if status --is-interactive
   eval (keychain --agents ssh --clear --eval id_rsa)
 end
 
+# Replace the vi mode prompt indicator with an empty function to disable it
+function fish_mode_prompt; end
+
 # powerline | powerline/powerline {{{1
 
 set fish_function_path $fish_function_path "$HOME/.pyenv/versions/3.6.3/lib/python3.6/site-packages/powerline/bindings/fish"
