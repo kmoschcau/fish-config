93a1ed2de4a35da873ee84a61c2fa1fac8751739
Kai Moschcau <kai.moschcau@sabre.com>
2018-03-15 09:59:52 +0100

    Revert "Add function to mount Trust netztemp"
    This reverts commit 9ad426a4407e3c97d4b0aa0e06c592fe32cebf4e.

--- functions/mount_netztemp.fish
+++
@@ -1,3 +0,0 @@
-function mount_netztemp --description 'Mounts the Trust netztemp samba share to /mnt/netztemp'
-  command sudo mount -t cifs -o 'domain=TRUST,user=kmoschcau,vers=1.0' //frafileserver1/netztemp/ /mnt/netztemp/
-end
