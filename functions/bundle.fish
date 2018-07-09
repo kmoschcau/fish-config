32bff7c7ca15c421343f6ad1e6633a4ff8ed8a2e
Kai Moschcau <kai.moschcau@sabre.com>
2018-07-09 11:25:45 +0200

    Use env in the bundle function instead of locals

--- functions/bundle.fish
+++ functions/bundle.fish
@@ -1,5 +1,3 @@
 function bundle --description 'Ruby bundler with proxy disabled'
-  set -l http_proxy
-  set -l https_proxy
-  command bundle $argv
+  env http_proxy='' https_proxy='' bundle $argv
 end
