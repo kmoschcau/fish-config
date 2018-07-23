bac3e3456cd69703986b2c85dcdd1d6a18f4f739
Kai Moschcau <kai.moschcau@sabre.com>
2018-07-23 09:30:57 +0200

    Remove redundant proxy disabled command wrappers

--- functions/bundle.fish
+++
@@ -1,3 +0,0 @@
-function bundle --description 'Ruby bundler with proxy disabled'
-  env http_proxy='' https_proxy='' bundle $argv
-end
--- functions/gem.fish
+++
@@ -1,3 +0,0 @@
-function gem --description 'Ruby gem with proxy disabled'
-  env http_proxy='' https_proxy='' gem $argv
-end
