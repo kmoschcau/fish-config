4eab241dd2f89ba9b9a7bae245d90cafd21118d0
Kai Moschcau <kai.moschcau@sabre.com>
2019-10-07 17:40:39 +0200

    Improve variable naming in deduplicate_path

--- functions/deduplicate_path.fish
+++ functions/deduplicate_path.fish
@@ -1,8 +1,8 @@
 function deduplicate_path --description 'Removes duplicate entries from $PATH'
   set -l new_path
-  for p in $PATH
-    if not contains $p $new_path
-      set new_path $new_path $p
+  for path_element in $PATH
+    if not contains $path_element $new_path
+      set new_path $new_path $path_element
     end
   end
   set PATH $new_path
