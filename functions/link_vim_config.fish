894236915f6856b9302916eb79962179edc9b8b6
Kai Moschcau <kai.moschcau@sabre.com>
2019-10-24 10:57:26 +0200

    Add function to link vim config files

---
+++ functions/link_vim_config.fish
@@ -0,0 +1,11 @@
+function link_vim_dir --description 'Create links to approriate nvim files in ~/.vim/'
+  mkdir --parents ~/.vim/autoload/
+
+  ln --symbolic ~/.config/nvim/autoload/airline/ ~/.vim/autoload/airline
+  ln --symbolic ~/.local/share/nvim/site/autoload/plug.vim ~/.vim/autoload/plug.vim
+  ln --symbolic ~/.config/nvim/coc-settings.json ~/.vim/coc-settings.json
+  ln --symbolic ~/.config/nvim/colors/ ~/.vim/colors
+  ln --symbolic ~/.config/nvim/ftplugin/ ~/.vim/ftplugin
+  ln --symbolic ~/.local/share/nvim/plugged/ ~/.vim/plugged
+  ln --symbolic ~/.config/nvim/init.vim ~/.vimrc
+end
