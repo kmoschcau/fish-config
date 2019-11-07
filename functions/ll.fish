6b57730ab5aa69ff849abb3526492d3004d52c1a
Kai Moschcau <kai.moschcau@sabre.com>
2019-11-07 14:58:55 +0100

    Add --group-directories-first option to ll

--- functions/ll.fish
+++ functions/ll.fish
@@ -1,4 +1,4 @@
 function ll --description 'll with default options'
-  command ls --all --color=auto --classify --human-readable -l\
-             --hide-control-chars $argv
+  command ls --all --color=auto --classify --group-directories-first\
+             --human-readable -l --hide-control-chars $argv
 end
