08177bbbe24b45402040130fddbd2addae15c28f
Kai Moschcau <kai.moschcau@sabre.com>
2019-11-07 15:56:07 +0100

    Add a guard to bwu for not installed bw

--- functions/bwu.fish
+++ functions/bwu.fish
@@ -1,3 +1,8 @@
 function bwu --description 'Run `bw unlock (read -s)` with a fancy prompt'
+  if not command --search --quiet bw
+    echo 'bw is not installed!'
+    exit 1
+  end
+
   bw unlock (read --silent --prompt=bw_password_prompt)
 end
