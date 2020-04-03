16227d86fb53e95290f7937ff3f5363250cb87e3
Kai Moschcau <kai.moschcau@sabre.com>
2020-04-03 16:37:08 +0200

    Improve use of highlight and cat in fzf_preview

--- functions/fzf_preview.fish
+++ functions/fzf_preview.fish
@@ -2,7 +2,12 @@ function fzf_preview --description 'Generates a preview for fzf'
   if string match --quiet --regex binary (file --mime $argv[1])
     echo $argv[1]' is a binary file'
   else
-    highlight --out-format truecolor --line-numbers $argv[1] 2> /dev/null;
-    or cat $argv[1] 2> /dev/null
+    if command --search highlight &> /dev/null
+      highlight --out-format truecolor \
+                --line-numbers \
+                --force $argv[1] 2> /dev/null;
+    else
+      cat $argv[1] 2> /dev/null
+    end
   end
 end
