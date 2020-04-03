870bf1951b39e5b3a91d0fa1f4b9f22fe2a66f2a
Kai Moschcau <kai.moschcau@sabre.com>
2020-04-03 12:53:08 +0200

    Change bwu to set BW_SESSION on its own

--- functions/bwu.fish
+++ functions/bwu.fish
@@ -1,8 +1,9 @@
-function bwu --description 'Run `bw unlock (read -s)` with a fancy prompt'
+function bwu --description 'Run `bw unlock (read -s)` with a fancy prompt and set BW_SESSION'
   if not command --search --quiet bw
     echo 'bw is not installed!'
     exit 1
   end
 
-  bw unlock (read --silent --prompt=bw_password_prompt)
+  set --global --export BW_SESSION \
+      (bw unlock --raw (read --silent --prompt=bw_password_prompt))
 end
