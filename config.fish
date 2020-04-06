a0d80b7a14de8275a4f39ae3e9fb77ade0d69a3d
Kai Moschcau <kai.moschcau@sabre.com>
2020-04-06 14:53:44 +0200

    Improve contrast for comments, autosuggest and compl-desc

--- config.fish
+++ config.fish
@@ -26,7 +26,7 @@ set --local grey_50         fafafa
 set --local grey_100        f5f5f5
 set --local grey_200        eeeeee
 set --local grey_300        e0e0e0
-set --local grey_400        bdbdbd
+set --local grey_500        9e9e9e
 set --local grey_700        616161
 
 # syntax colors for the shell {{{2
@@ -37,13 +37,13 @@ set fish_color_redirection    $yellow_700
 set fish_color_end            $orange_600
 set fish_color_error          $red_500
 set fish_color_param          $cyan_600
-set fish_color_comment        $grey_400
+set fish_color_comment        $grey_500
 set fish_color_match          $teal_200
 set fish_color_selection      --background=$cyan_100
 set fish_color_search_match   --background=$yellow_500
 set fish_color_operator       $teal_400 --bold
 set fish_color_escape         $light_green_600
-set fish_color_autosuggestion $grey_400
+set fish_color_autosuggestion $grey_500
 set fish_color_cancel         --background=$grey_200 --italics
 
 # prompt highlights {{{2
@@ -57,7 +57,7 @@ set fish_pager_color_progress             $green_700 --background=$grey_100 --bo
 set fish_pager_color_background           --background=$grey_100
 set fish_pager_color_prefix               $blue_600 --underline --bold
 set fish_pager_color_completion           $grey_700
-set fish_pager_color_description          $grey_400
+set fish_pager_color_description          $grey_500
 set fish_pager_color_prefix               $blue_700 --underline --bold
 set fish_pager_color_selected_background  --background=$cyan_100
 
