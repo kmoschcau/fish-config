f80980282e2e5eac51e762a7345a6455a2e34721
Kai Moschcau <kai.moschcau@sabre.com>
2020-11-12 13:30:30 +0100

    Change fish syntax highlight to match vim

--- config.fish
+++ config.fish
@@ -8,19 +8,19 @@ end
 
 # colors {{{1
 # color definitions {{{2
-set --local red_500       f44336
+set --local red_500         f44336
+set --local red_600         e53935
 set --local blue_600        1e88e5
 set --local blue_700        1976d2
 set --local cyan_100        b2ebf2
-set --local cyan_600        00acc1
 set --local teal_200        80cbc4
-set --local teal_400        26a69a
+set --local teal_500        009688
 set --local green_600       43a047
 set --local green_700       388e3c
 set --local light_green_600 7cb342
 set --local yellow_500      ffeb3b
-set --local yellow_700      fbc02d
 set --local amber_600       ffb300
+set --local orange_500      ff9800
 set --local orange_600      fb8c00
 set --local grey_50         fafafa
 set --local grey_100        f5f5f5
@@ -31,17 +31,17 @@ set --local grey_700        616161
 
 # syntax colors for the shell {{{2
 set fish_color_normal         $grey_700
-set fish_color_command        $orange_600 --bold
+set fish_color_command        $teal_500
 set fish_color_quote          $green_600
-set fish_color_redirection    $yellow_700
-set fish_color_end            $orange_600
-set fish_color_error          $red_500
-set fish_color_param          $cyan_600
+set fish_color_redirection    $orange_600
+set fish_color_end            $red_600
+set fish_color_error          $red_500 --underline
+set fish_color_param          $orange_500 --italics
 set fish_color_comment        $grey_500
 set fish_color_match          $teal_200
 set fish_color_selection      --background=$cyan_100
 set fish_color_search_match   --background=$yellow_500
-set fish_color_operator       $teal_400 --bold
+set fish_color_operator       $orange_600 --bold
 set fish_color_escape         $light_green_600
 set fish_color_autosuggestion $grey_500
 set fish_color_cancel         --background=$grey_200 --italics
