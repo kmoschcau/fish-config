c8ca2d248f74fcddafdf1c779315a3e6555e2c06
Kai Moschcau <kai.moschcau@sabre.com>
2021-02-08 16:48:15 +0100

    Switch fzf preview to bat

--- functions/fzf_preview.fish
+++ functions/fzf_preview.fish
@@ -2,10 +2,11 @@ function fzf_preview --description 'Generates a preview for fzf'
   if string match --quiet --regex binary (file --mime $argv[1])
     echo $argv[1]' is a binary file'
   else
-    if command --search highlight &> /dev/null
-      highlight --out-format truecolor \
-                --line-numbers \
-                --force $argv[1] 2> /dev/null;
+    if command --search bat &> /dev/null
+      bat \
+         --color always \
+         --italic-text always \
+         $argv[1] 2> /dev/null
     else
       cat $argv[1] 2> /dev/null
     end
