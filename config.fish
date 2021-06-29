88eff1870eeba5c7f80ab969b87b2d581529d00d
Moschcau, Kai <kai.moschcau@sabre.com>
2021-06-29 12:59:19 +0200

    Update to fish 3.3.0

--- config.fish
+++ config.fish
@@ -38,27 +38,27 @@ set fish_color_end            $red_600
 set fish_color_error          $red_500 --underline
 set fish_color_param          $orange_500 --italics
 set fish_color_comment        $grey_500
-set fish_color_selection      --background=$cyan_100
-set fish_color_search_match   --background=$yellow_500
+set fish_color_selection      --background $cyan_100
+set fish_color_search_match   --background $yellow_500
 set fish_color_operator       $orange_600 --bold
 set fish_color_escape         $light_green_600
 set fish_color_autosuggestion $grey_500
-set fish_color_cancel         --background=$grey_200 --italics
+set fish_color_cancel         --background $grey_200 --italics
 
 # prompt highlights {{{2
-set fish_color_cwd         $grey_50 --background=$grey_700
-set fish_color_user        $grey_50 --background=$grey_300
-set fish_color_host        $grey_50 --background=$grey_300
-set fish_color_host_remote $grey_50 --background=$amber_600
+set fish_color_cwd         $grey_50 --background $grey_700
+set fish_color_user        $grey_50 --background $grey_300
+set fish_color_host        $grey_50 --background $grey_300
+set fish_color_host_remote $grey_50 --background $amber_600
 
 # completion pager highlights {{{2
-set fish_pager_color_progress             $green_700 --background=$grey_100 --bold
-set fish_pager_color_background           --background=$grey_100
+set fish_pager_color_progress             $green_700 --background $grey_100 --bold
+set fish_pager_color_background           --background $grey_100
 set fish_pager_color_prefix               $blue_600 --underline --bold
 set fish_pager_color_completion           $grey_700
 set fish_pager_color_description          $grey_500
 set fish_pager_color_prefix               $blue_700 --underline --bold
-set fish_pager_color_selected_background  --background=$cyan_100
+set fish_pager_color_selected_background  --background $cyan_100
 
 # cursor shapes for the different vi modes {{{1
 set fish_cursor_default     block      blink
