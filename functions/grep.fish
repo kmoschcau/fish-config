d8efdfbf8a7180d7a7619c96fd049a6e4f77c8ba
Kai Moschcau <kai.moschcau@blecon.de>
2023-10-06 09:34:14 +0200

    fix: grep being incompatible on macOS

--- functions/grep.fish
+++ functions/grep.fish
@@ -1,5 +1,18 @@
 function grep \
     --description 'grep with default options' \
     --wraps grep
-    command grep --color=auto --label=STDIN --line-number --initial-tab $argv
+    if string match --regex --quiet "BSD grep" (command grep --version)
+        command grep \
+                --color=auto \
+                --label=STDIN \
+                --line-number \
+                $argv
+    else
+        command grep \
+                --color=auto \
+                --label=STDIN \
+                --line-number \
+                --initial-tab \
+                $argv
+    end
 end
