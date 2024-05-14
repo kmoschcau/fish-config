f2ddb237577fceb476317f5a9220696284471be4
Kai Moschcau <kai.moschcau@blecon.de>
2024-05-14 09:37:37 +0200

    refactor: vi mode prompt function

--- functions/fish_mode_prompt.fish
+++ functions/fish_mode_prompt.fish
@@ -1,38 +1,41 @@
 function fish_mode_prompt \
-    --description 'Write out the current mode in the prompt'
-    if test "$fish_key_bindings" = fish_vi_key_bindings
-        or test "$fish_key_bindings" = fish_hybrid_key_bindings
+    --description 'Write out the current mode in the prompt.'
 
-        # switch on current mode
-        set --local background
-        switch $fish_bind_mode
-            case default
-                set background $material_cyan_A700
-                set_color --bold --background $background $material_grey_50
-                echo -n ' N '
-            case insert
-                set background $material_blue_600
-                set_color --bold --background $background $material_grey_50
-                echo -n ' I '
-            case replace
-                set background $material_amber_600
-                set_color --bold --background $background $material_grey_50
-                echo -n ' R '
-            case replace_one
-                set background $material_amber_600
-                set_color --bold --background $background $material_grey_50
-                echo -n ' r '
-            case visual
-                set background $material_cyan_100
-                set_color --bold --background $background $material_grey_700
-                echo -n ' V '
-        end
+    if not contains $fish_key_bindings \
+        fish_vi_key_bindings fish_hybrid_key_bindings
 
-        # write separator
-        set_color $background --background $material_grey_100
-        echo -n ''
-
-        # reset colors
-        set_color normal
+        return
     end
+
+    # switch on current mode
+    set --local background
+    switch $fish_bind_mode
+        case default
+            set background $material_cyan_A700
+            set_color --bold --background $background $material_grey_50
+            echo -n ' N '
+        case insert
+            set background $material_blue_600
+            set_color --bold --background $background $material_grey_50
+            echo -n ' I '
+        case replace
+            set background $material_amber_600
+            set_color --bold --background $background $material_grey_50
+            echo -n ' R '
+        case replace_one
+            set background $material_amber_600
+            set_color --bold --background $background $material_grey_50
+            echo -n ' r '
+        case visual
+            set background $material_cyan_100
+            set_color --bold --background $background $material_grey_700
+            echo -n ' V '
+    end
+
+    # write separator
+    set_color $background --background $material_grey_100
+    echo -n ''
+
+    # reset colors
+    set_color normal
 end
